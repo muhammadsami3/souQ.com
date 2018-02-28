81807a2e3c7f50a92b8fe2b6f89745b7c7a42670
muhammadsami3 <34835192+muhammadsami3@users.noreply.github.com>
2018-02-28 15:46:36 +0200

    admin edit gui

--- souQ/src/java/filters/adminpageFilter.java
+++ souQ/src/java/filters/adminpageFilter.java
@@ -6,9 +6,6 @@
 package filters;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -30,17 +27,14 @@ public class adminpageFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
